a3319d7e886fd9dcba18304d9f17ba8307ba99df
Hau Le <haule.it92@gmail.com>
2024-01-16 16:02:02 +0700

    update site features

--- client/components/partials/homepage/home-default/SiteFeatures.jsx
+++ client/components/partials/homepage/home-default/SiteFeatures.jsx
@@ -1,75 +1,74 @@
-import React from 'react'
+import React from "react";
 
 const SiteFeatures = () => (
-  <div className='ps-site-features'>
-    <div className='ps-container'>
-      <div className='ps-block--site-features'>
-        <div className='ps-block__item'>
-          <div className='ps-block__left'>
-            <i className='icon-rocket'></i>
+  <div className="ps-site-features">
+    <div className="ps-container">
+      <div className="ps-block--site-features">
+        <div className="ps-block__item">
+          <div className="ps-block__left">
+            <i className="icon-rocket"></i>
           </div>
-          <div className='ps-block__right'>
-            <h4>Set up your wallet</h4>
+          <div className="ps-block__right">
+            <h4>Configure your Metamask for Seamless Transactions</h4>
             <p>
-              Once you have set your Metamask wallet and connected it
-              to your account, by pressing connect button you can
-              start selling or buying.
+              Link your wallet to your account. Click the “Connect Wallet”
+              button to initiate transactions for buying or selling.
             </p>
           </div>
         </div>
-        <div className='ps-block__item'>
-          <div className='ps-block__left'>
-            <i className='icon-sync'></i>
+        <div className="ps-block__item">
+          <div className="ps-block__left">
+            <i className="icon-sync"></i>
           </div>
-          <div className='ps-block__right'>
-            <h4>List items for sale</h4>
+          <div className="ps-block__right">
+            <h4>Showcase or Explore</h4>
             <p>
-              List your items for sale and start creating an extra
-              income or business and connect with people globally{' '}
+              All available products on a Global Marketplace. Initiate income
+              generation by presenting your products to a diverse audience of
+              international buyers.
             </p>
           </div>
         </div>
-        <div className='ps-block__item'>
-          <div className='ps-block__left'>
-            <i className='icon-credit-card'></i>
+        <div className="ps-block__item">
+          <div className="ps-block__left">
+            <i className="icon-credit-card"></i>
           </div>
-          <div className='ps-block__right'>
+          <div className="ps-block__right">
             <h4>Shop with Confidence</h4>
             <p>
-              Our buyer protection covers your purchase to delivery
-              employing smart contracts on the blockchain. No funds
-              are held by Flyblox{' '}
+              Our buyer protection covers your purchase to delivery employing
+              smart contracts on the blockchain. No funds are held by Flyblox{" "}
             </p>
           </div>
         </div>
-        <div className='ps-block__item'>
-          <div className='ps-block__left'>
-            <i className='icon-cloud-check'></i>
+        <div className="ps-block__item">
+          <div className="ps-block__left">
+            <i className="icon-cloud-check"></i>
           </div>
-          <div className='ps-block__right'>
-            <h4>Wallet security</h4>
+          <div className="ps-block__right">
+            <h4>Safeguard Wallet Security with Metamask</h4>
             <p>
-              Meta mask is open source and you control your private
-              keys with a secure login. You are always in control when
-              interacting on the new decentralized web.
+              An open-source self-custody wallet. Retain control of your private
+              keys through a secure login, empowering you to navigate the
+              decentralized web.
             </p>
           </div>
         </div>
-        <div className='ps-block__item'>
-          <div className='ps-block__left'>
-            <i className='icon-apartment'></i>
+        <div className="ps-block__item">
+          <div className="ps-block__left">
+            <i className="icon-apartment"></i>
           </div>
-          <div className='ps-block__right'>
-            <h4>World wide shopping</h4>
+          <div className="ps-block__right">
+            <h4>Facilitating Global Commerce</h4>
             <p>
-              We help connect people from all over the world to create
-              a global market place in trading goods and services
+              Uniting individuals worldwide, fostering a marketplace for trading
+              diverse goods and services on a global scale
             </p>
           </div>
         </div>
       </div>
     </div>
   </div>
-)
+);
 
-export default SiteFeatures
+export default SiteFeatures;
